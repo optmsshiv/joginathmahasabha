cfc52d348a5c2c32b915b25f614765b7b421b9ed
optmsshiv <optmsshiv@gmail.com>
2025-07-16 22:01:44 +0530

    `Added responsive design and mobile view styles to CSS and updated HTML structure to accommodate hamburger menu and floating donate/WhatsApp buttons.`

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+
+  window.addEventListener("load", function () {
+    const loader = document.getElementById("preloader");
+    loader.style.opacity = "0";
+    loader.style.visibility = "hidden";
+    loader.style.transition = "opacity 0.5s ease";
+  });
+
